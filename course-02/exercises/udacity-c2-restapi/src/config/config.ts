07a4585df91d56b3e8a605ecfabc06e4ef4a731c
sv3nson <sven.volgmann@gmail.com>
2023-02-13 11:06:39 +0100

    initial

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udavolgmanndev",
+    "password": "udavolgmanndev",
+    "database": "udavolgmanndev",
+    "host": "udagramvolgmannd.c3subglmiw4b.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagramvolgmannd-bucket"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
